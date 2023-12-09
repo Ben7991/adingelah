dea02969d3bbb3e384922e5e1aa288c2b02370f2
Bernard Teye <datssosh@gmail.com>
2023-12-08 21:57:43 -0700

    add go back component to be used acrossed several pages

---
+++ resources/views/components/go-back.blade.php
@@ -0,0 +1,12 @@
+<div class="container-fluid mb-4 p-0">
+    <div class="row">
+        <div class="col-12 col-lg-5 col-xl-4 col-xxl-3">
+            <div class="go-back">
+                <a href="{{ $path }}" class="go-back__link shadow-sm rounded-pill">
+                    <i class="bi bi-arrow-left"></i>
+                </a>
+                <p class="m-0">{{ $title }}</p>
+            </div>
+        </div>
+    </div>
+</div>
