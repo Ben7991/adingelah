ece6b3a2a74ffe8a4524603c2c2fd37f7ccd0577
Bernard Teye <datssosh@gmail.com>
2023-12-09 14:28:04 -0700

    add index, create and edit route actions to UpcomingEventsController.php file

--- app/Http/Controllers/UpcomingEventsController.php
+++ app/Http/Controllers/UpcomingEventsController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class UpcomingEventsController extends Controller
 {
-    //
+    public function index() {
+        return view("events.index");
+    }
+
+    public function create() {
+        return view("events.create");
+    }
+
+    public function edit($id) {
+        return view("events.edit");
+    }
 }
