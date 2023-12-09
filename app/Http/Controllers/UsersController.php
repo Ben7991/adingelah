58a3537122e2f72163cd9dc9f194f4367a90aa32
Bernard Teye <datssosh@gmail.com>
2023-12-08 23:39:38 -0700

    add create route action to UsersController.php file

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,4 +9,8 @@ class UsersController extends Controller
     public function index() {
         return view("users.index");
     }
+
+    public function create() {
+        return view("users.create");
+    }
 }
