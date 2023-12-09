3544c88af79c3ee10ea7968418c93bf3b419d75b
Bernard Teye <datssosh@gmail.com>
2023-12-08 22:33:57 -0700

    change favicon in tab

--- resources/views/components/layout/admin.blade.php
+++ resources/views/components/layout/admin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Adingelah Foundation | Admin</title>
-    <link rel="shortcut icon" href="{{ asset("assets/img/logo.jpg") }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset("assets/img/logo-big.png") }}">
     <link rel="stylesheet" href="{{ asset("assets/bootstrap/css/bootstrap.min.css") }}">
     <link rel="stylesheet" href="{{ asset("assets/bootstrap-icons/font/bootstrap-icons.min.css") }}">
     <link rel="stylesheet" href="{{ asset("assets/css/general.css") }}">
