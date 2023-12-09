96bcd0561d9d306b5308b5bd55c8da9e205ef0c1
Bernard Teye <datssosh@gmail.com>
2023-12-08 21:43:33 -0700

    add create route action to CategoriesController

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -9,4 +9,8 @@ class CategoriesController extends Controller
     public function index() {
         return view("categories.index");
     }
+
+    public function create() {
+        return view("categories.create");
+    }
 }
