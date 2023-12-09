e651b6688feb83955bc29e2760064271957e3cae
Bernard Teye <datssosh@gmail.com>
2023-12-09 07:58:43 -0700

    complete create.blade.php and edit.blade.php files for both post create and edit pages

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,54 @@
+<x-layout.admin>
+    <x-slot name="title">Posts</x-slot>
+    @push("styles")
+        <!-- Include stylesheet -->
+        <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+    @endpush
+
+    <x-go-back path="/admin/posts" title="Posts"></x-go-back>
+
+    <div class="container-fluid p-0">
+        <div class="row">
+            <div class="col-12 col-md-4">
+                <div class="text-center overflow-hidden image-holder rounded">
+                    <p class="my-5 mx-2">Post banner will show here when you upload the file</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-8">
+                <div class="border rounded bg-white shadow-sm p-3">
+                    <h5 class="mt-0 mb-3">Edit Post</h5>
+                    <form action="/admin/posts" method="post">
+                        <div class="form-group mb-3">
+                            <label for="title" class="mb-1">Title</label>
+                            <input type="text" name="title" id="title" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="banner" class="mb-1">Banner</label>
+                            <input type="file" name="banner" id="banner" class="form-control">
+                        </div>
+                        <div class="form-group mb-4">
+                            <label for="description" class="mb-1">Description</label>
+                            <!-- Create the editor container -->
+                            <div id="editor"></div>
+                        </div>
+                        <button class="btn btn-main px-4 rounded-pill">
+                            <i class="bi bi-save"></i>&nbsp;Save changes
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @push("scripts")
+        <!-- Main Quill library -->
+        <script src="//cdn.quilljs.com/1.3.6/quill.min.js"></script>
+
+        <script>
+            var quill = new Quill('#editor', {
+              theme: 'snow',
+              placeholder: "Type blog message here"
+            });
+          </script>
+    @endpush
+</x-layout.admin>
