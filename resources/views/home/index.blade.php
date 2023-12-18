6fa0b3264a694fbafa0ca94f2f667ad47fd6dd46
Bernard Teye <datssosh@gmail.com>
2023-12-18 10:37:54 -0700

    add home layout file and folder to hold all home pages

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Testing</title>
+    <link rel="stylesheet" href="{{ asset("assets/css/general.css") }}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,400;6..12,600;6..12,700&family=Open+Sans:wght@300;400;600;800&display=swap">
+</head>
+<body>
+    <p>Testing with a paragraph tag</p>
+</body>
+</html>
